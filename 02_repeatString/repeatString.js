0608db7a5d0ed0471e48b53365888836e692369c
shinotaur <71105630+shinotaur@users.noreply.github.com>
2022-10-19 14:51:15 +0530

    Add 01_helloWorld/  02_repeatString/ 03_reverseString/

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let input = "";
+  for (let i = 0; i < num; i++) {
+    input = input + string;
+  }
+  return input;
 };
 
 // Do not edit below this line
